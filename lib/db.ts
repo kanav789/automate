fa175d6decaa0f21b0537ba2b41521a299c15403
kanav789 <kanavrana91@gmail.com>
2025-06-25 20:35:54 +0530

    1:45

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from "@lib/generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
-export const prisma = globalThis.prisma || new PrismaClient();
+export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db
